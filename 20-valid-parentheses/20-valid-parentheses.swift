61a9c4bf1c60b5f61dec82293bae4d6b92f32081
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-04-18 20:20:47 -0400

    Time: 0 ms (100.00%), Space: 14.3 MB (50.52%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.swift
+++ 20-valid-parentheses/20-valid-parentheses.swift
@@ -1,27 +1,16 @@
 class Solution {
     func isValid(_ s: String) -> Bool {
         var stack = [Character]()
-        for letter in s {
-            // ((()))
-            if let last = stack.last, letter == compliment(last) {
-                _ = stack.removeLast()
+        for letter in Array(s) {
+            if let last = stack.last,
+                ((last == "[" && letter == "]")
+                || (last == "{" && letter == "}")
+                || (last == "(" && letter == ")")) {
+                    stack.removeLast()
             } else {
-                stack.append(letter)   
+                stack.append(letter)
             }
         }
         return stack.isEmpty
-    }
-
-    func compliment(_ char: Character) -> Character {
-        switch char {
-        case "(":
-            return ")"
-        case "[":
-            return "]"
-        case "{":
-            return "}"
-        default:
-            return " "
-        }
     }
 }
