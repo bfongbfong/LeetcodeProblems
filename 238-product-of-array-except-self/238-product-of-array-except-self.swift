fac2f3719fb853f8d79cd53db11c62e73b4c7e0a
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-04-23 23:33:53 -0400

    Time: 187 ms (61.96%), Space: 20.2 MB (96.74%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.swift
+++ 238-product-of-array-except-self/238-product-of-array-except-self.swift
@@ -2,12 +2,7 @@ class Solution {
     func productExceptSelf(_ nums: [Int]) -> [Int] {
         var prefix = Array(repeating: 0, count: nums.count)
         var postfix = Array(repeating: 0, count: nums.count)
-        var prefixProd = 1
         var answer = [Int]()
-        for i in 0..<nums.count {
-            prefix[i] = prefixProd
-            prefixProd *= nums[i]
-        }
 
         var postfixProd = 1
         for i in stride(from: nums.count - 1, through: 0, by: -1) {
@@ -15,9 +10,13 @@ class Solution {
             postfixProd *= nums[i]
         }
 
+        var prefixProd = 1
         for i in 0..<nums.count {
+            prefix[i] = prefixProd
+            prefixProd *= nums[i]
             answer.append(prefix[i] * postfix[i])
         }
+
         return answer
     }
 }
