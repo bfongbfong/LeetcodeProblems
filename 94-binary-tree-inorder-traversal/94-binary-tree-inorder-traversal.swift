218f5160864387eeca69c37f29ed2260c9437a74
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-04-24 17:52:10 -0400

    Time: 8 ms (18.62%), Space: 14.1 MB (67.02%) - LeetHub

---
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.swift
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+        var answer = [Int]()
+        func helper(_ root: TreeNode?) {
+            guard let root = root else { return }
+            helper(root.left)
+            answer.append(root.val)
+            helper(root.right)
+        }
+        helper(root)
+        return answer
+    }
+}
