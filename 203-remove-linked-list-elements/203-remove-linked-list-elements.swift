820340c3f3b7cbc048e74cb96a7ffe8261e85753
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-04-26 17:50:22 -0400

    Time: 55 ms (70.77%), Space: 15.6 MB (86.67%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.swift
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+        var dummyHead = ListNode(0)
+        dummyHead.next = head
+        var curr = head
+        var prev: ListNode? = dummyHead
+        while curr != nil {
+            if curr?.val == val {
+                prev?.next = curr?.next
+            } else {
+                prev = curr
+            }
+            curr = curr?.next
+        }
+        return dummyHead.next
+    }
+}
