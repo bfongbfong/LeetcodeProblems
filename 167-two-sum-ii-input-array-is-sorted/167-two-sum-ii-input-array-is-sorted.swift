a2bc7731b5efbf133a05d8dc726c75f3dc90bda5
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-04-28 17:17:14 -0400

    Time: 79 ms (69.25%), Space: 14.6 MB (81.24%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.swift
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.swift
@@ -1,15 +1,19 @@
 class Solution {
     func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
-        var leftI = 0
-        var rightI = numbers.count - 1
-        while leftI < rightI {
-            let sum = numbers[leftI] + numbers[rightI]
-            if sum == target {
-                return [leftI + 1, rightI + 1]
-            } else if sum < target {
-                leftI += 1
-            } else if sum > target {
-                rightI -= 1
+        for i in 0..<numbers.count {
+            let compliment = target - numbers[i]
+            var leftI = i + 1
+            var rightI = numbers.count - 1
+            while leftI <= rightI {
+                let mid = leftI + ((rightI - leftI) / 2)
+                if numbers[mid] == compliment {
+                    return [i + 1, mid + 1]
+                } else if compliment > numbers[mid] {
+                    // go right
+                    leftI = mid + 1
+                } else {
+                    rightI = mid - 1
+                }
             }
         }
         return []
