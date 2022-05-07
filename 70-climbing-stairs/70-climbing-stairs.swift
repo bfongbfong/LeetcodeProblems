dfae336866b6c0f48638a85f4f24f8b59f0fe573
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-05-07 14:26:09 -0400

    Time: 3 ms (54.62%), Space: 13.5 MB (90.14%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.swift
+++ 70-climbing-stairs/70-climbing-stairs.swift
@@ -1,27 +1,6 @@
 class Solution {
     func climbStairs(_ n: Int) -> Int {
         if n == 1 { return 1 }
-        /*
-        1 - 1
-        2 - 2
-        3 - 3
-        4 - 5
-            1 1 1 1 
-            1 2 1
-            2 1 1
-            1 1 2
-            2 2
-        5 - 8
-            1 1 1 1 1 
-            2   1 1 1 
-            1 2   1 1
-            1 1 2.  1
-            1 1 1 2
-            2   2.  1
-            2.  1  2
-            1 2   2
-        */
-
         var dp = Array(repeating: 0, count: n + 1)
         dp[0] = 1
         dp[1] = 1
