462a0e2dd10c2c09e875c165ec64b747eec1325b
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-05-08 23:56:39 -0400

    Time: 109 ms (21.84%), Space: 22.6 MB (21.26%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.swift
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.swift
@@ -11,13 +11,13 @@ class Solution {
         fbcde
         ace
         */
-        var memo = [[Int]: Int]()
+        var memo = Array(repeating: Array(repeating: -1, count: text2.count), count: text1.count)
         func helper(_ index1: Int, _ index2: Int) -> Int {
-            guard memo[[index1, index2]] == nil else { return memo[[index1, index2]]! }
+            guard memo[index1][index2] == -1 else { return memo[index1][index2] }
             if text1[index1] == text2[index2] {
                 if index1 + 1 < text1.count && index2 + 1 < text2.count {
                     let answer = helper(index1 + 1, index2 + 1) + 1
-                    memo[[index1, index2]] = answer
+                    memo[index1][index2] = answer
                    return answer
                 } else {
                     return 1
@@ -32,7 +32,7 @@ class Solution {
                     two = helper(index1, index2 + 1)
                 }
                 let answer = max(one, two)
-                memo[[index1, index2]] = answer
+                memo[index1][index2] = answer
                 return answer
             }
         }
