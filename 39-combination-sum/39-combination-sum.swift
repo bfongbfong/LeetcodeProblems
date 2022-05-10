9dd8f9fc341c342331b4a9ddba3790dc28c0c5bd
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-05-09 22:37:54 -0400

    Time: 32 ms (28.72%), Space: 13.7 MB (98.69%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        var answer = [[Int]]()
+        func helper(_ i: Int, _ currentNums: [Int]) {
+            let sum = currentNums.reduce(0, +)
+            // print(i, currentNums)
+
+            if sum == target {
+                // add the answer here
+                answer.append(currentNums)
+                return
+            } else if sum > target {
+                // noop
+            } else {
+                // keep going further down 
+                var nums = currentNums
+                nums.append(candidates[i])
+                helper(i, nums)
+            }
+            if i + 1 < candidates.count {
+                helper(i + 1, currentNums)
+            }
+            
+        }
+        helper(0, [])
+        return answer
+    }
+}
