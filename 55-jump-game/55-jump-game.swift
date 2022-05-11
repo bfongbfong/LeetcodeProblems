72158ea1cab7c32f47cb0ba6903f81dec29d5a00
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-05-11 10:03:13 -0400

    Time: 1945 ms (9.26%), Space: 14.6 MB (25.07%) - LeetHub

--- 55-jump-game/55-jump-game.swift
+++ 55-jump-game/55-jump-game.swift
@@ -1,18 +1,20 @@
 class Solution {
     func canJump(_ nums: [Int]) -> Bool {
-        var memo = [Int: Bool]()
-        func dfs(_ i: Int) -> Bool {
-            if i >= nums.count { return false }
-            if i == nums.count - 1 { return true }
-            if memo[i] != nil { return memo[i]! }
-            for jump in stride(from: nums[i], through: 1, by: -1) {
-                if dfs(i + jump) {
-                    return true
+        var dp = Array(repeating: false, count: nums.count)
+        for i in stride(from: nums.count - 1, through: 0, by: -1) {
+            if i == nums.count - 1 {
+                dp[i] = true
+            } else {
+                var result = false
+                for jump in stride(from: nums[i], through: 1, by: -1) {
+                    if i + jump < nums.count && dp[i + jump] {
+                        result = true
+                        break
+                    }
                 }
+                dp[i] = result
             }
-            memo[i] = false
-            return false
         }
-        return dfs(0)
+        return dp[0]
     }
 }
