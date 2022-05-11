cc75db6ba32fc2024221d98d4ed0e880d96f1c30
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-05-11 19:27:24 -0400

    Time: 26 ms (6.39%), Space: 14.1 MB (76.50%) - LeetHub

--- 46-permutations/46-permutations.swift
+++ 46-permutations/46-permutations.swift
@@ -4,25 +4,24 @@ class Solution {
         1            2           3 4 5
         2 3 4 5      1 3 4 5
         */
-        func dfs(_ included: [Int], _ includedSet: Set<Int>) -> [[Int]] {
+        func dfs(_ included: [Int], _ numbers: [Int]) -> [[Int]] {
             // either add your index or skip it
-            if includedSet.count == nums.count {
+            if included.count == nums.count {
                 return [included]
             }
 
             var answer: [[Int]] = []
 
-            for i in 0..<nums.count {
-                if includedSet.contains(i) { continue }
+            for i in 0..<numbers.count {
                 var included = included
-                included.append(nums[i])
-                var includedSet = includedSet
-                includedSet.insert(i)
-                answer.append(contentsOf: dfs(included, includedSet))
+                included.append(numbers[i])
+                var numbers = numbers
+                numbers.remove(at: i)
+                answer.append(contentsOf: dfs(included, numbers))
             }
             return answer
         }
 
-        return dfs([], [])
+        return dfs([], nums)
     }
 }
