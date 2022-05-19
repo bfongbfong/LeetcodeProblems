e284af75d31d8e8c77d3c05f3b902e0bf3574548
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-05-19 13:57:08 -0400

    Time: 42 ms (29.85%), Space: 14.1 MB (71.64%) - LeetHub

--- 256-paint-house/256-paint-house.swift
+++ 256-paint-house/256-paint-house.swift
@@ -1,24 +1,30 @@
 class Solution {
     func minCost(_ costs: [[Int]]) -> Int {
-        /*
-        houses  red     blue    green
-        0       17      2       17
-        1       16      16      5
-        2       14      3       19
+        /* red blue gree
+        0  17   2.   17
+        1  16.  16.  5
+        2  14.  3.   19
         */
 
-        var dp = Array(repeating: Array(repeating: Int.max, count: costs[0].count), count: costs.count)
-        dp.append([0, 0, 0])
-        for houseI in stride(from: costs.count - 1, through: 0, by: -1) {
-            for color in 0..<costs[houseI].count {
-                var minChildrenSum = Int.max
-                for nextColor in 0..<dp[houseI + 1].count {
-                    if nextColor == color { continue }
-                    minChildrenSum = min(minChildrenSum, dp[houseI + 1][nextColor])
+        var memo = Array(repeating: Array(repeating: -1, count: costs[0].count), count: costs.count)
+        func helper(_ row: Int, _ prevColor: Int) -> Int {
+            var answer = Int.max
+            for color in 0..<costs[row].count {
+                if color == prevColor { continue }
+                if row + 1 < costs.count {
+                    if memo[row + 1][color] != -1 { 
+                        answer = min(answer, memo[row + 1][color] + costs[row][color])
+                    } else {
+                        let next = helper(row + 1, color)
+                        memo[row + 1][color] = next
+                        answer = min(answer, next + costs[row][color])
+                    }
+                } else {
+                    answer = min(answer, costs[row][color])
                 }
-                dp[houseI][color] = minChildrenSum + costs[houseI][color]
             }
+            return answer
         }
-        return min(min(dp[0][0], dp[0][1]), dp[0][2])
+        return helper(0, -1)
     }
 }
