ebf168d59080a2d63f9b571429eed5518b20e3dc
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-05-19 11:40:41 -0400

    Time: 114 ms (20.44%), Space: 16.1 MB (90.75%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.swift
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.swift
@@ -14,43 +14,23 @@
 
 class Solution {
     func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
-        /*
-        i could keep an array for each downward traversal.
-        if i reach one of the numbers, then i 
-        eventually, I'll have both arrays. loop through both arrays backwards until i get to a different number. 
-        */
-        var pPath = [TreeNode]()
-        var qPath = [TreeNode]()
-        func helper(_ root: TreeNode?, _ path: [TreeNode]) {
-            guard let root = root else { return }
-            var path = path
-            path.append(root)
-            if root === q {
-                qPath = path
-                if !pPath.isEmpty {
-                    return
-                }
-            } else if root === p {
-                pPath = path
-                if !qPath.isEmpty {
-                    return
-                }
+        var answer: TreeNode? = TreeNode(0)
+        func hasOne(_ root: TreeNode?) -> Bool {
+            guard let root = root else { return false }
+            let leftHasOne = hasOne(root.left)
+            let rightHasOne = hasOne(root.right)
+            var rootIsOne = false
+            if root === p || root === q { 
+                rootIsOne = true
             }
+            if (leftHasOne && rightHasOne) || (rootIsOne && (leftHasOne || rightHasOne)) {
+                answer = root
+                return true
+            }
+            return leftHasOne || rightHasOne || rootIsOne
+        }
 
-            helper(root.left, path)
-            helper(root.right, path)
-        }
-        helper(root, [])
-        var i = 0
-        var lca: TreeNode?
-        while i < pPath.count && i < qPath.count {
-            if pPath[i] === qPath[i] {
-                lca = pPath[i]
-                i += 1
-            } else {
-                break
-            }
-        }
-        return lca
+        _ = hasOne(root)
+        return answer
     }
 }
