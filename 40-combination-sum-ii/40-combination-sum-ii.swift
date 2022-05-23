f440345101c8ae4866a15fe3d399593455c30583
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-05-23 19:38:45 -0400

    Time: 22 ms (18.25%), Space: 14.4 MB (29.20%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func combinationSum2(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        var answer = [[Int]]()
+        var candidates = candidates.sorted()
+        // used is a set of indexes
+        func helper(_ i: Int, _ target: Int, _ curr: [Int]) {
+            var curr = curr
+            if target == 0 {
+                answer.append(curr)
+            } else if target < 0 { 
+                return 
+            }
+            var prev = -1
+            for j in i..<candidates.count {
+                if candidates[j] == prev { continue }
+                curr.append(candidates[j])
+                helper(j + 1, target - candidates[j], curr)
+                curr.removeLast()
+                prev = candidates[j]
+            }
+        }
+        helper(0, target, [])
+        return answer
+    }
+}
