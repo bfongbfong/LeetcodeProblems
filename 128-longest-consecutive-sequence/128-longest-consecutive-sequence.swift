352f9f506b7e84abb8ccfc04335b3568140b6b79
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2022-05-26 21:03:54 -0400

    Time: 556 ms (39.25%), Space: 27.9 MB (5.00%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.swift
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.swift
@@ -1,40 +1,22 @@
 class Solution {
     func longestConsecutive(_ nums: [Int]) -> Int {
-        /*
-        iterate through and build an adjacency list
-        and then traverse the list to see the longest uninterrupted path
-        100: []
-        4: 3
-        200: []
-        1: 2
-        3: 2
-        2: 1, 3
-        [[1], [1, 2], [6, 8], ]
-        0
-        */
         var set = Set<Int>()
         for num in nums {
             set.insert(num)
         }
 
-        var processed = Set<Int>()
-        func helper(_ num: Int, _ prev: Int? = nil) -> Int {
-            processed.insert(num)
-            var answer = 1
-            if set.contains(num + 1) && num + 1 != prev {
-                answer += helper(num + 1, num)
+        func consecutiveLength(_ num: Int) -> Int {
+            if set.contains(num) {
+                set.remove(num)
+                return 1 + consecutiveLength(num + 1) + consecutiveLength(num - 1)
+            } else {
+                return 0
             }
-            if set.contains(num - 1) && num - 1 != prev {
-                answer += helper(num - 1, num)
-            }
-            return answer
         }
 
-        var answer = 0
-        for num in nums {
-            if !processed.contains(num) {
-                answer = max(answer, helper(num))
-            }
+        var answer = 0 
+        for num in set {
+            answer = max(answer, consecutiveLength(num))
         }
         return answer
     }
